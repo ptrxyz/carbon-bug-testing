bf8b54aac439d2b463d25a9e0dc83e6a126ede00
Peter Krauß <peter.krauss@kit.edu>
2023-01-25 04:00:17 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import 'carbon-components-svelte/css/white.css'
+
+    import { Dropdown } from 'carbon-components-svelte'
+</script>
+
+<Dropdown
+    titleText="Contact"
+    selectedId="0"
+    items={[
+        { id: '0', text: 'Slack' },
+        { id: '1', text: 'Email' },
+        { id: '2', text: 'Fax' },
+    ]}
+/>
